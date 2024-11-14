ebb91cdf4864a298dd3492192a353bc7e591f83e
neta schwarz <netaschwarz@Shemer-MacBookPro-2023.local>
2024-11-14 14:14:00 +0200

    fixed

--- FVCalc.java
+++ FVCalc.java
@@ -7,6 +7,6 @@ public class FVCalc {
 		double rate = percent/100;
 		double d = Math.pow((1+rate), years);
 		double futureValue = currentValue * d;
-		System.out.println("After " + years + " years, $" + currentValue + " saved at "+ percent + "% will yield $" +((int)futureValue));
+		System.out.println("After " + years + " years, a $" + currentValue + " saved at "+ percent + "% will yield $" +((int)futureValue));
 	}
 }
