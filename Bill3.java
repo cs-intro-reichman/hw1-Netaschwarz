ca11538e72469ce893ed5aca6766426eeb933fff
neta schwarz <netaschwarz@Shemer-MacBookPro-2023.local>
2024-11-14 14:11:55 +0200

    fixed

--- Bill3.java
+++ Bill3.java
@@ -6,6 +6,6 @@ public class Bill3 {
 		String name3 = args[2];
 		int sum = Integer.parseInt(args[3]);
 		double money = Math.round((sum/3.0) +0.49);//add 0.49 so it will round up in the case of sum/3 is not a whole number (instead of rounding down)
-		System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 + ": pay " + money + " Shekels each.");
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + money + " Shekels each.");
 	}
 }
